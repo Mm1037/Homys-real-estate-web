4e82dec8d5b108aec63c4fb0988febc9e7f1055a
Moaz Mohammed <138526864+Mm1037@users.noreply.github.com>
2025-01-21 06:22:05 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./componants/Header/Header";
+import Hero from "./componants/Hero/Hero";
+import './App.css'
+import Companies from "./componants/Companies/Companies";
+import Residencies from "./componants/Residencies/Residencies";
+import Value from "./componants/Value/Value";
+import Contact from "./componants/Contact/Contact";
+import GetStarted from "./componants/GetStarted/GetStarted";
+import Footer from "./componants/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <div className="white-gradient"/>
+      <Header/>
+      <Hero/>
+      </div>
+      <Companies/>
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
+    </div>
+
+  );
+}
+
+
+export default App;
---
+++ src/componants/GetStarted/GetStarted.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./GetStarted.css";
+function GetStarted() {
+  return (
+    <section className="g-wrapper">
+      <div id="started" className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started With Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+          <button className="button">
+            <a href="moazmohamed.tech@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default GetStarted;
